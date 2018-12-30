91f1889cd6d983faf51bb8d08be84cac7a360448
Ratin_Kumar <ratin.kumar.2k@gmail.com>
2018-12-30 20:30:34 +0530

    Harrison now uses an auto_canny function

--- harrison.py
+++ harrison.py
@@ -26,11 +26,11 @@ def roi(image , vertices):
 
 def process(image):
     pimage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    pimage = cv2.Canny(pimage , threshold1 = 200 , threshold2 = 300)
+    pimage = auto_canny(pimage , threshold1 = 200 , threshold2 = 300)
     vertices =np.array([[0,800],[10,500],[400,300],[620,300],[1200,800]])
     g = roi(pimage , [vertices])
     
-    lines = cv2.HoughLinesP(pimage , 1 , np.pi/180 , 180 , 20 ,15 )
+    lines = cv2.HoughLinesP(pimage , 1 , np.pi/180 , 180 ,np.array([]), 20 ,15 )
     draw_lines(g ,lines)
     return g
 
@@ -39,12 +39,28 @@ def process(image):
 cap = cv2.VideoCapture("test2.mp4")
 while(cap.isOpened()):
     ret, frame = cap.read()
-    image = process(frame)
-    cv2.imshow("image", image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if ret:
+        image = process(frame)  
+        cv2.imshow("image", image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        print("[INFO] End of Video Stream")
         break
 cap.release()
 cv2.destroyAllWindows()
+
+def auto_canny(image, sigma=0.01):
+	# compute the median of the single channel pixel intensities
+	v = np.median(image)
+ 
+	# apply automatic Canny edge detection using the computed median
+	lower = int(max(0, (1.0 - sigma) * v))
+	upper = int(min(255, (1.0 + sigma) * v))
+	edged = cv2.Canny(image, lower, upper)
+ 
+	# return the edged image
+	return edged
 
 
 
